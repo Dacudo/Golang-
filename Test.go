9f60d327aff3a276c84244713e1535017b5992a5
Dacudo <58285071+Dacudo@users.noreply.github.com>
2020-04-13 15:58:53 -0500

    A file meant to test a func
    For some reason both functions fail one only one should.

---
+++ Test.go
@@ -0,0 +1,35 @@
+package prose
+
+import (
+	"strings"
+	"testing"
+)
+
+//JoinWithCommas will link words
+func JoinWithCommas(phrases []string) string {
+	result := strings.Join(phrases[:len(phrases)-1], ",")
+
+	result += ", and"
+
+	result += phrases[len(phrases)-1]
+
+	return result
+}
+
+func TestTwoElements(t *testing.T) {
+	list := []string{"apple", "orange"}
+
+	if JoinWithCommas(list) != "apple and orange" {
+
+		t.Error("Didn't match expected value")
+	}
+}
+
+func TestThreeElements(t *testing.T) {
+	list := []string{"apple, orange, pear"}
+
+	if JoinWithCommas(list) != "apple, orange, and pear" {
+
+		t.Error("Didn't match expected value")
+	}
+}
